4ef15da01c4e5d96c8511e10190b5e034d8dd9ba
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-05-14 01:14:53 +0900

    [FEAT] 리뷰 생성 및 경우별 페이징 구현

--- src/main/java/ahchacha/ahchacha/dto/ReviewDto.java
+++ src/main/java/ahchacha/ahchacha/dto/ReviewDto.java
@@ -25,15 +25,20 @@ public class ReviewDto {
     @Getter
     @Setter
     @Builder
-    public static class ReviewResponseDto { //내가 등록한 물품을 빌린 사람에 대해
+    public static class ReviewResponseDto {
         private Long reviewId;
         private String reviewComment;
         private BigDecimal reviewScore;
 
+        private Long itemOwnerId;
+        private String ownerNickName;
+        private String ownerProfile;
+
         private Long renterUserId;
-        private PersonType personType;
         private String renterNickName;
         private String renterProfile;
+
+        private PersonType personType;
 
         private LocalDateTime createdAt;
         private LocalDateTime updatedAt;
@@ -44,10 +49,15 @@ public class ReviewDto {
                     .reviewComment(review.getReviewComment())
                     .reviewScore(review.getReviewScore())
 
-//                    .personType(PersonType.RENTER)
-                    .personType(review.getPersonType())
+                    .itemOwnerId(review.getItemOwnerId())
+                    .ownerNickName(review.getOwnerNickName())
+                    .ownerProfile(review.getOwnerProfile())
+
+                    .renterUserId(review.getRenterUserId())
                     .renterNickName(review.getRenterNickName())
                     .renterProfile(review.getRenterProfile())
+
+                    .personType(review.getPersonType())
 
                     .createdAt(review.getCreatedAt())
                     .updatedAt(review.getUpdatedAt())
@@ -57,44 +67,5 @@ public class ReviewDto {
 
     public static Page<ReviewResponseDto> toDtoPage(Page<Review> itemReviewPage) {
         return itemReviewPage.map(ReviewResponseDto::toDto);
-    }
-
-
-    //////////////////////////////////////
-    @Getter
-    @Setter
-    @Builder
-    public static class ReviewRentedResponseDto { //내가 반납완료한 아이템의 주인에게 리뷰
-        private Long reviewId;
-        private String reviewComment;
-        private BigDecimal reviewScore;
-
-        private Long itemOwnerId;
-        private PersonType personType;
-        private String ownerNickName;
-        private String ownerProfile;
-
-        private LocalDateTime createdAt;
-        private LocalDateTime updatedAt;
-
-        public static ReviewRentedResponseDto toDto(Review review) {
-            return ReviewRentedResponseDto.builder()
-                    .reviewId(review.getId())
-                    .reviewComment(review.getReviewComment())
-                    .reviewScore(review.getReviewScore())
-
-                    .itemOwnerId(review.getItemOwnerId())
-                    .personType(review.getPersonType())
-                    .ownerNickName(review.getOwnerNickName())
-                    .ownerProfile(review.getOwnerProfile())
-
-                    .createdAt(review.getCreatedAt())
-                    .updatedAt(review.getUpdatedAt())
-                    .build();
-        }
-    }
-
-    public static Page<ReviewRentedResponseDto> toDtoPageRented(Page<Review> itemReviewPage) {
-        return itemReviewPage.map(ReviewRentedResponseDto::toDto);
     }
 }
