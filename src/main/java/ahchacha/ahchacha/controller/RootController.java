77745073150b8fd08ddfe70a7519e7edc4dbff1a
DongUk <mywayx1205@naver.com>
2024-05-21 16:34:02 +0900

    [FIX] healthCheck 코드 변경

--- src/main/java/ahchacha/ahchacha/controller/RootController.java
+++ src/main/java/ahchacha/ahchacha/controller/RootController.java
@@ -1,14 +1,16 @@
 package ahchacha.ahchacha.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
-    @GetMapping("/health")
-    public String healthCheck(){
-        return "I'm healthy!";
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck(){
+        return new ResponseEntity<>("I'm healthy!", HttpStatus.OK);
     }
 
 }
