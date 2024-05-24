96698c8658e57327b2b703094450ae3aeb2b1df3
uk1205 <128315369+uk1205@users.noreply.github.com>
2024-05-24 19:44:04 +0900

    Update User.java

--- src/main/java/ahchacha/ahchacha/domain/User.java
+++ src/main/java/ahchacha/ahchacha/domain/User.java
@@ -75,4 +75,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Community> communities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Authentication> authentication = new ArrayList<>();
 }
