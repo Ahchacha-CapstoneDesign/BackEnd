8edda172b575b4266c1fd2b6f6a70d2ebdf31b18
uk1205 <128315369+uk1205@users.noreply.github.com>
2024-05-24 19:54:06 +0900

    Delete src/main/java/ahchacha/ahchacha/repository/ChatRoomRepository.java

--- src/main/java/ahchacha/ahchacha/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package ahchacha.ahchacha.repository;
-
-import ahchacha.ahchacha.domain.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    ChatRoom findByItemId(Long itemId);
-}
