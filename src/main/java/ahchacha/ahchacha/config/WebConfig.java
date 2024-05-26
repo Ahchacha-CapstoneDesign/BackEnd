e717842437da87266f795576bf3a3a54e2f431ec
DongUk <mywayx1205@naver.com>
2024-05-26 23:23:45 +0900

    [FIX] Cors관련 수정

--- src/main/java/ahchacha/ahchacha/config/WebConfig.java
+++ src/main/java/ahchacha/ahchacha/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("ahchacha.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
