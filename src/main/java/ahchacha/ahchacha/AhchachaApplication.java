ca25fd8498f8c355c209247ba00f1e3a403ece15
DongUk <mywayx1205@naver.com>
2024-05-29 13:28:25 +0900

    [FIX] main 타임존 삭제

--- src/main/java/ahchacha/ahchacha/AhchachaApplication.java
+++ src/main/java/ahchacha/ahchacha/AhchachaApplication.java
@@ -1,23 +1,14 @@
 package ahchacha.ahchacha;
 
-import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.TimeZone;
-
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class AhchachaApplication {
-
-	@PostConstruct
-	void started() {
-		// timezone UTC 셋팅
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AhchachaApplication.class, args);
